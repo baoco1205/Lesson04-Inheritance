b75664d5e46e6c86d8e694df579fb02b59ae95a2
Baoco15254516894 <co.letruongbao@students.hueic.edu.vn>
2018-09-11 16:05:57 +0700

    Thuc thi phuong thuc dienTich()

--- src/co/dev/Square.java
+++ src/co/dev/Square.java
@@ -1,5 +1,17 @@
 package co.dev;
 
 public class Square extends Shape {
+	private double chieuDai;
+	private double chieuRong;
+	
+	public Square(double chieuDai, double chieuRong) {
+		this.chieuDai= chieuDai;
+		this.chieuRong= chieuRong;
+	}
 
+	@Override
+	public double dienTich() {
+		// TODO Auto-generated method stub
+		return chieuDai*chieuRong;
+	}
 }
